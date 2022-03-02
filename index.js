056946156431d2a4412b4f21c00dc70131488a77
unknown <AnikBarua34>
2022-03-02 21:35:43 +0600

    port change

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const port =process.env.PORT || 9000;
+
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: port,
     methods: ["GET", "POST"],
   },
 });
@@ -31,6 +33,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(port, () => {
   console.log("SERVER RUNNING");
 });
